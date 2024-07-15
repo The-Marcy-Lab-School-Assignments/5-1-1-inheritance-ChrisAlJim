0f1f1c9e373cdbc64df8ae19fb2207198ecbe4aa
Jahlapeno1 <jimenezaot@gmail.com>
2024-07-15 16:04:31 -0400

    finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,33 @@
 class Quadrilateral {
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+    this.side4 = side4
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4
+  }
 
 }
 
-class Rectangle {
+class Rectangle extends Quadrilateral{
+  constructor(side1, side2) {
+    super(side1, side2, side1, side2)
+  }
+  getArea() {
+    return this.side1 * this.side2
+  }
 
+  getDiagonal() {
+    return Math.sqrt(this.side1 ** 2 + this.side2 ** 2)
+  }
 }
 
-class Square {
-
+class Square extends Rectangle {
+  constructor(side1) {
+    super(side1, side1, side1, side1)
+  }
 }
 
 /* Be creative with this one! */
@@ -21,3 +41,4 @@ module.exports = {
   Square,
   Person,
 };
+
